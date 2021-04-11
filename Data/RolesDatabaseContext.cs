7a3fd679cf8168308d37a6631b39eb8600ca9c37
Laurentiu <dodoloilaurentiu@gmail.com>
2021-04-11 14:45:35 +0300

    feat: create database context

---
+++ Data/RolesDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RoleService.Models;
+
+namespace RoleService.Data
+{
+    public class RolesDatabaseContext : DbContext
+    {
+        public RolesDatabaseContext(DbContextOptions<RolesDatabaseContext> options) : base(options)
+        {
+            //
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<UserPermission> UserPermissions { get; set; }
+    }
+}
