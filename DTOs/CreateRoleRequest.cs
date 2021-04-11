5fe2a7480b9596d7ec4a37921f22559c3110d6cc
Laurentiu <dodoloilaurentiu@gmail.com>
2021-04-11 14:45:53 +0300

    feat: add requests for role

---
+++ DTOs/CreateRoleRequest.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using RoleService.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RoleService.DTOs
+{
+    public class CreateRoleRequest : IRequest<Role>
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Field is empty")]
+        public string Name { get; set; }
+
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ DTOs/DeleteRoleRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using RoleService.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RoleService.DTOs
+{
+    public class DeleteRoleRequest: IRequest<Role>
+    {
+        public Guid Id { get; set; }
+    }
+}
