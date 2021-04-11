83d5eb3bc3edd8e1615f4e5fb9a0feab6dd11c5d
Laurentiu <dodoloilaurentiu@gmail.com>
2021-04-11 16:04:45 +0300

    fix: remove extra classes

--- Data/RolesDatabaseContext.cs
+++ Data/RolesDatabaseContext.cs
@@ -12,8 +12,5 @@ namespace RoleService.Data
 
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
-        public DbSet<Permission> Permissions { get; set; }
-        public DbSet<UserRole> UserRoles { get; set; }
-        public DbSet<UserPermission> UserPermissions { get; set; }
     }
 }
