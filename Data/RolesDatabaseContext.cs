57eb931d75443f2b7d5e6e9201920a55b5dc027f
Laurentiu <dodoloilaurentiu@gmail.com>
2021-04-11 16:28:22 +0300

    feat: add many-to-many relationship between user and role

--- Data/RolesDatabaseContext.cs
+++ Data/RolesDatabaseContext.cs
@@ -5,13 +5,18 @@ namespace RoleService.Data
 {
     public class RolesDatabaseContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
         public RolesDatabaseContext(DbContextOptions<RolesDatabaseContext> options) : base(options)
         {
             //
         }
 
-        public DbSet<User> Users { get; set; }
-        public DbSet<Role> Roles { get; set; }
-        public DbSet<UserRole> UserRoles { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserRole>().HasKey(userRole => new { userRole.UserId, userRole.RoleId });
+        }
     }
 }
